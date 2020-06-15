e667ce451fc8f55cb23d2435ee0ad593cc2979ff
Jamie <me@jcox.tech>
2020-06-15 21:14:18 +0000

    Add some comments

--- app/blueprints/cron.py
+++ app/blueprints/cron.py
@@ -12,15 +12,23 @@ cron_blueprint = Blueprint(name='cron',
                            url_prefix='/cron')
 
 
+@cron_blueprint.route('/sync', methods=['POST'])
+def sync_db_with_sheet():
+    values = get_values_from_sheet()
+    set_values_to_database(values)
+    return 'OK', 200
+
+
 def set_values_to_database(values):
     db = cron_blueprint.config['DB']
     db_name = os.environ['DB_NAME']
     args = [iter(values)] * 500  # firestore limit of operations
-    for group in zip_longest(*args):
+    for group in zip_longest(*args):  # use grouper pattern to batch process
         batch = db.batch()
         for entry in group:
             if entry is not None:
                 try:
+                    # something datastore friendly as a unique id (NB: could auto generate but we want to replace existing)
                     key = str('-'.join(list(entry['name'].strip().replace('/', '-').split(' ')) + [entry['location'].replace('/', '-').strip()])).lower()
                 except KeyError:
                     continue
@@ -40,6 +48,7 @@ def get_values_from_sheet():
     values = []
     for row in result.get('values', []):
         obj = {}
+        # NB: sheet headers may change!
         for i, field in enumerate(['name', 'location', 'type', 'link', 'genre', 'notes']):
             try:
                 obj[field] = row[i]
@@ -47,10 +56,3 @@ def get_values_from_sheet():
                 obj[field] = ''  # some fields may be empty which truncates the row data
             values.append(obj)
     return values
-
-
-@cron_blueprint.route('/sync', methods=['POST'])
-def sync_db_with_sheet():
-    values = get_values_from_sheet()
-    set_values_to_database(values)
-    return 'OK', 200
