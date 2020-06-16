d7354d018d38350982e0d36b1c442f7b1ef7280d
Kieran Alington <kieran@mumuagency.com>
2020-06-16 20:07:18 +0100

    Add Genres

--- app/blueprints/api.py
+++ app/blueprints/api.py
@@ -41,3 +41,22 @@ def locations():
     
     #set() unique-ifies the list, then convert back to list so it can be jsonify'd
     return jsonify(list(set(locations)))
+
+
+@api_blueprint.route('/genres', methods=['GET'])
+def genres():
+    db = api_blueprint.config['DB']
+    db_name = os.environ['DB_NAME']
+
+    entries = db.collection(db_name).get()
+
+    genres = []
+
+    for entry in entries:
+        item = entry.to_dict()
+
+        for genre in item['genre']:
+            genres.append(genre.strip().title())
+    
+    #set() unique-ifies the list, then convert back to list so it can be jsonify'd
+    return jsonify(list(set(genres)))
