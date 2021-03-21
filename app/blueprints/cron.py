8d6ebd1a6a3d91008001832e1dbb8b040c84b165
Jamie <me@jcox.tech>
2021-03-21 01:18:47 +0000

    WIP sync creatives sheet data to new db

--- app/blueprints/cron.py
+++ app/blueprints/cron.py
@@ -30,7 +30,7 @@ def auth_check(func):
 @auth_check
 def remove_old_entries():
     db = cron_blueprint.config['DB']
-    db_name = os.environ['DB_NAME']
+    db_name = os.environ['MUSIC_DB_NAME']
 
     entries = db.collection(db_name)
     entries = entries.where('timestamp', '<', datetime.utcnow() - timedelta(hours=24))
@@ -41,13 +41,36 @@ def remove_old_entries():
     return 'OK', 200
 
 
-@cron_blueprint.route('/sync', methods=['POST'])
+@cron_blueprint.route('/sync-music', methods=['POST'])
 @auth_check
-def sync_db_with_sheet():
+def sync_db_with_music_sheet():
+    db_name = os.environ['MUSIC_DB_NAME']
     current_app.logger.debug('Sync requested...')
-    values = get_values_from_sheet()
-    current_app.logger.debug(f'Adding {len(values)} to database...')
-    set_values_to_database(values)
+    values = get_values_from_sheet(
+        os.environ['MUSIC_SHEET_ID'],
+        os.environ['MUSIC_TAB_ID'],
+        os.environ['MUSIC_START_ROW'],
+        os.environ['MUSIC_SHEET_HEADERS']
+    )
+    current_app.logger.debug(f'Adding {len(values)} to {db_name} database...')
+    set_values_to_database(db_name, values)
+    return 'OK', 200
+
+
+
+@cron_blueprint.route('/sync-creatives', methods=['POST'])
+@auth_check
+def sync_db_with_creatives_sheet():
+    db_name = os.environ['CREATIVES_DB_NAME']
+    current_app.logger.debug('Sync requested...')
+    values = get_values_from_sheet(
+        os.environ['CREATIVES_SHEET_ID'],
+        os.environ['CREATIVES_TAB_ID'],
+        os.environ['CREATIVES_START_ROW'],
+        os.environ['CREATIVES_SHEET_HEADERS']
+    )
+    current_app.logger.debug(f'Adding {len(values)} to {db_name} database...')
+    set_values_to_database(db_name, values)
     return 'OK', 200
 
 
@@ -55,7 +78,7 @@ def sync_db_with_sheet():
 @auth_check
 def scrape_bandcamp():
     db = cron_blueprint.config['DB']
-    db_name = os.environ['DB_NAME']
+    db_name = os.environ['MUSIC_DB_NAME']
     publisher = cron_blueprint.config['PUBLISHER']
     topic_name = f'projects/{os.environ["PROJECT_ID"]}/topics/{os.environ["SCRAPE_TOPIC"]}'
 
@@ -72,12 +95,11 @@ def scrape_bandcamp():
     return 'OK', 200
 
 
-def set_values_to_database(values: List[dict]):
+def set_values_to_database(db_name, values: List[dict]):
     """
     Perform batches of writes to a Firestore database.
     """
     db = cron_blueprint.config['DB']
-    db_name = os.environ['DB_NAME']
     # Firestore limit of operations (500: 2 per row: 1 write; 1 timestamp)
     args = [iter(values)] * 250
     new_entries = 0
@@ -125,7 +147,7 @@ def set_values_to_database(values: List[dict]):
     current_app.logger.info(f'Sync to database complete: {total_entries} entries from Sheet ({new_entries} new)')
 
 
-def get_values_from_sheet():
+def get_values_from_sheet(sheet_id, tab_id, start_row, headers) -> List[dict]:
     """
     Use the Google Sheets API to fetch and process rows of data.
 
@@ -139,17 +161,72 @@ def get_values_from_sheet():
         service = build('sheets', 'v4', credentials=credentials)
     sheet = service.spreadsheets()
     # NB: if sheet headers change the capture range may have to change
-    sheet_range = f"{os.environ['TAB_ID']}!A{os.environ['START_ROW']}:H"
-    result = sheet.values().get(spreadsheetId=os.environ['SHEET_ID'],
+    sheet_range = f"{tab_id}!A{start_row}:H"
+    result = sheet.values().get(spreadsheetId=sheet_id,
                                 range=sheet_range).execute()
 
     # NB: sheet headers may change!
-    headers = ['name', 'country', 'city', 'state', 'type', 'link', 'genre', 'notes']
+    headers = map(str.strip, headers.split(","))
+
     return [
-        row
-        for value in result.get('values', [])
-        if (row := process_row(value, headers)) is not None
+        obj
+        for row in result.get('values', [])
+        if (obj := process_row(row, headers)) is not None
     ]
+
+
+class ProcessingError(Exception):
+    pass
+
+
+def process_links(value: str):
+    return [process_link(url) for url in value.split()]
+
+
+def process_link(value: str):
+    """
+    www.bandname.bandcamp.com -> https://bandname.bandcamp.com
+    """
+    value = value.strip().replace('"', '')
+    match = re.match("www\.([a-zA-Z0-9]+\.bandcamp\.com)", value)
+    if match is not None:
+        value = f"https://{match.group(1)}"
+    return value
+
+
+def process_name_first_letter(value: str):
+    try:
+        return (
+            value[0].lower() 
+            if value[0].isalpha() 
+            else "#"
+        )
+    except IndexError:
+        raise ProcessingError('Row missing value for `name`')
+
+
+def process_location(value: str):
+    return "UK" if value.title() == "England" else value
+
+
+def process_location_concat(city: str, state: str, country: str):
+    return ', '.join(e.replace('/', ',') for e in [city, state, country] if e)
+
+
+def process_location_tags(value: str):
+    # Normalise locations allow for separation with slashes rather than columns
+    return [part.lower().strip() for part in value.split(',') if part]
+
+
+def process_genre_tags(value: str):
+    # Normalise genres allow for separation with slashes rather than columns
+    genres = value.replace('#', ',').replace('/', ',').split(',')
+    return [genre.lower().strip() for genre in genres if genre]
+
+
+def process_field(value: str):
+    fields = value.replace('/', ',').split(',')
+    return [field.title().strip() for field in fields if field]
 
 
 def process_row(row: tuple, headers_in_order: list):
@@ -168,22 +245,17 @@ def process_row(row: tuple, headers_in_order: list):
         except IndexError:
             obj[field] = ''  # some fields may be empty which truncates the row data
 
-    # fixes issues where submissions mistakenly type e.g.: www.bandname.bandcamp.com
-    match = re.match('www\.([a-zA-Z0-9]+\.bandcamp\.com)', obj['link'])
-    if match is not None:
-        obj['link'] = f'https://{match.group(1)}'
-
-    try:
-        obj['name_first_letter'] = obj['name'][0].lower() if obj['name'][0].isalpha() else '#'
-    except IndexError:
-        current_app.logger.warn(f'Row missing name value (not saved): {row}')
+    try:    
+        obj["name_first_letter"] = process_name_first_letter(obj["name"])
+        obj["link"] = process_link(obj.get("link", ""))
+        obj["links"] = process_links(obj.get("links", ""))
+        obj["location"] = process_location(obj.get("country", "") or obj.get("location", ""))
+        obj["location"] = process_location_concat(obj["city"], obj["state"], obj["location"])
+        obj["location_tags"] = process_location_tags(obj["location"])
+        obj["genre_tags"] = process_genre_tags(obj.get("genre", ""))
+        obj["type"] = obj.get("type") or process_field(obj.get("field", ""))
+    except (KeyError, ProcessingError) as e:
+        current_app.logger.warn(f'Processing row {row} failed (not saved): {e.message}')
         return
-
-    # Normalise genres and locations, allow for separation with slashes rather than columns
-    # TODO: this is a temp fix for splitting location into individual columns
-    obj['location'] = ', '.join(e.replace('/', ',') for e in [obj["city"], obj["state"], obj["country"]] if e)
-    genres = obj.get('genre', '').replace('#', ',').replace('/', ',').split(',')
-    obj['genre_tags'] = [genre.lower().strip() for genre in genres if genre]
-    obj['location_tags'] = [part.lower().strip() for part in obj.get('location', '').split(',') if part]
 
     return obj
