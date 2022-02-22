94f1e707bb35406bdd196243256510278c0ec4a4
leo <leo@numberly.com>
2022-02-22 10:58:25 +0100

    update vue config proxy to prod url

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     outputDir: "dist",
     assetsDir: "static",
     devServer: {
-        proxy: 'https://blackbandcamp-creatives-v2-g5yujcfoqq-ew.a.run.app/',
+        proxy: 'https://blackartistdatabase.co/',
         disableHostCheck: true
       }
 };
